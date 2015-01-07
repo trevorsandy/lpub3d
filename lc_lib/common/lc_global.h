ccc9ae71f6091fbb95e0a181b43f796b34ea3a66
Trevor <trevor.sandy@gmail.com>
2015-01-07 07:47:38 +0000

    Build 1711

---
+++ lc_lib/common/lc_global.h
@@ -0,0 +1,40 @@
+#ifndef _LC_GLOBAL_H_
+#define _LC_GLOBAL_H_
+
+#include "lc_config.h"
+
+// Version number.
+#define LC_VERSION_MAJOR 0
+#define LC_VERSION_MINOR 80
+#define LC_VERSION_PATCH 4
+#define LC_VERSION_TEXT "0.80.4"
+#define LC_VERSION_BUILD "1710"
+
+// Forward declarations.
+class lcModel;
+class lcObject;
+class lcPiece;
+class lcCamera;
+class lcLight;
+class lcGroup;
+class PieceInfo;
+struct lcPartsListEntry;
+struct lcModelPartsEntry;
+
+class lcVector2;
+class lcVector3;
+class lcVector4;
+class lcMatrix44;
+
+class lcContext;
+class lcMesh;
+struct lcMeshSection;
+struct lcRenderMesh;
+class lcTexture;
+struct lcScene;
+
+class lcFile;
+class lcMemFile;
+class lcDiskFile;
+
+#endif // _LC_GLOBAL_H_
