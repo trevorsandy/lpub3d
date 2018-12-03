2123379c093a5d445255e7177a3869028b699f04
Trevor SANDY <trevor.sandy@gmail.com>
2018-12-03 06:24:15 +0100

    Dev - Upgrade lclib to LeoCAD commit 71b5797

--- lclib/common/lc_global.h
+++ lclib/common/lc_global.h
@@ -41,7 +41,7 @@ char* strlwr(char* string);
 #define LC_VERSION_PATCH 0
 #define LC_VERSION_TEXT "18.02"
 /*** LPub3D Mod - Git SHA ***/
-#define LC_VERSION_BUILD "7bb791e"
+#define LC_VERSION_BUILD "71b5797"
 /*** LPub3D Mod end ***/
 
 
--- mainApp/docs/README.txt
+++ mainApp/docs/README.txt
@@ -1,4 +1,4 @@
-LPub3D 2.3.4.1.1053 (03 12 2018 06:22:20)
+LPub3D 2.3.4.1.1054 (03 12 2018 06:24:16)
 
 Features, enhancements, fixes and changes
 ------------
